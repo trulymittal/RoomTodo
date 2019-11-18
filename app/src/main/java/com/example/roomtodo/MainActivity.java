645546f4fa3c2245d02512949f86c31274f7606c
Truly Mittal <trulymittal@gmail.com>
2019-11-18 18:11:33 +0530

    removed viewmodel button

--- app/src/main/java/com/example/roomtodo/MainActivity.java
+++ app/src/main/java/com/example/roomtodo/MainActivity.java
@@ -137,9 +137,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void getAllUsingViewModelAndLiveData(View view) {
-    }
-
     class InsertAsyncTask extends AsyncTask<Todo, Void, Void> {
 
         @Override
